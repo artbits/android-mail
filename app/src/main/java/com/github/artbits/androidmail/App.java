2f1e0b5fe82cd5d98e5d4e2270b23da58a0bf753
artbits <zguanhu@foxmail.com>
2023-04-29 14:10:59 +0800

    使用 QuickIO 数据库替换 LitePal ORM

--- app/src/main/java/com/github/artbits/androidmail/App.java
+++ app/src/main/java/com/github/artbits/androidmail/App.java
@@ -2,14 +2,21 @@ package com.github.artbits.androidmail;
 
 import android.app.Application;
 
-import org.litepal.LitePal;
+import com.github.artbits.quickio.api.DB;
+import com.github.artbits.quickio.core.Config;
+import com.github.artbits.quickio.core.QuickIO;
 
 public class App extends Application {
+
+    public static DB db;
+
 
     @Override
     public void onCreate() {
         super.onCreate();
-        LitePal.initialize(this);
+        String dbName = "store";
+        String basePath = getExternalFilesDir(null).getAbsolutePath();
+        db = QuickIO.usingDB(Config.of(c -> c.path(basePath).name(dbName)));
     }
 
 }
